59d327f03e3c00091d37a051601b9e822b9c9664
LaAmaDelSexo <andres.ojeda.inbox@gmail.com>
2026-03-15 13:55:20 -0400

    refactored fileSys & change resolution to 640 x360.

--- Main.cs
+++ Main.cs
@@ -5,11 +5,16 @@ public partial class Main : Node
 {
     [Export]
     public PackedScene BaseMob { get; set; }
-
+    [Export]
+    private CanvasLayer Hud;
     public override void _Ready()
     {
         Engine.MaxFps = 60;
         Engine.PhysicsTicksPerSecond = 60;
     }
 
+    public override void _Process(double delta)
+    {
+
+    }
 }
